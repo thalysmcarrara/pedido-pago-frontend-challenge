77dd5ea08b37f7782e4e41a76cc138b0cef97970
thalysmcarrara <thalysmatiascarrara@gmail.com>
2022-02-21 14:33:29 -0400

    feat: searchBar component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { useRouter } from 'next/router';
 import { Container, Hero } from './style.home';
-import { ActiveLink } from '../components';
+import { ActiveLink, SearchBar } from '../components';
 
 export default function Home() {
   const { asPath } = useRouter();
@@ -13,6 +13,8 @@ export default function Home() {
             <ActiveLink name="Colaboradores" href='/' actualPath={asPath}/> 
             <ActiveLink name='Cargos' href='/cargos' actualPath={asPath}/>
           </nav>
+
+          <SearchBar />
         </Hero>
       </Container>
   )
