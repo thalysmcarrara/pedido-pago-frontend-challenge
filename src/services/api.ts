722c17012539803e14038467af108ad385dec535
thalysmcarrara <thalysmatiascarrara@gmail.com>
2022-02-22 14:45:56 -0400

    fix: change api baseURL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'https://bbbff4e5-200b-406b-9650-e44c68439220.mock.pstmn.io'
+  baseURL: 'https://pp-api-desafio.herokuapp.com'
 })
